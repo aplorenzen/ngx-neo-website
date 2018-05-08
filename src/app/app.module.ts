6d0a681b9131d5395a6a3304bce8f48c3f709b3f
Andreas Lorenzen <aplorenzen@gmail.com>
2018-05-08 14:28:37 +0200

    Implemented scrollreveal for skills, implemented new menu dropdown logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { HomeModule } from './home/home.module';
 import { AboutModule } from './about/about.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NgsRevealModule } from 'ng-scrollreveal';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   imports: [
@@ -19,11 +22,13 @@ import { AppRoutingModule } from './app-routing.module';
     HttpClientModule,
     TranslateModule.forRoot(),
     NgbModule.forRoot(),
+    NgsRevealModule.forRoot(),
     CoreModule,
     SharedModule,
     HomeModule,
     AboutModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   declarations: [
     AppComponent
--- src/app/core/shell/navbar/navbar.component.ts
+++ src/app/core/shell/navbar/navbar.component.ts
@@ -1,24 +1,68 @@
-import {Component, Input} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 import { I18nService } from '../../i18n.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        display: 'none',
+        transform: 'translate3d(120%, 0, 0)'
+      })),
+      state('out', style({
+        display: 'block',
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      transition('in => out', animate('200ms ease-in-out')),
+      transition('out => in', animate('200ms ease-in-out'))
+    ]),
+  ]
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
-  menuHidden = true;
+  menuState = 'out';
+  mobileView = false;
 
   constructor(private i18nService: I18nService) {}
 
+  handleResize() {
+    if (window.innerWidth >= 576) {
+      this.menuState = 'out';
+      this.mobileView = false;
+    } else {
+      this.menuState = 'in';
+      this.mobileView = true;
+    }
+  }
+
+  ngOnInit() {
+    this.handleResize();
+  }
+
   toggleMenu() {
-    this.menuHidden = !this.menuHidden;
+    if (this.mobileView) {
+      this.menuState = this.menuState === 'out' ? 'in' : 'out';
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.handleResize();
+  }
+
+  hideMenu() {
+    if (this.mobileView) {
+      this.menuState = 'in';
+    }
   }
 
   setLanguage(language: string) {
     this.i18nService.language = language;
+    this.hideMenu();
   }
 
   get currentLanguage(): string {
@@ -28,5 +72,4 @@ export class NavbarComponent {
   get languages(): string[] {
     return this.i18nService.supportedLanguages;
   }
-
 }
