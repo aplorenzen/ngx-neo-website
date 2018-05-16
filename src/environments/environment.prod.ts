3a5365124c9b9bf170b3fe1e485b9221ed08cab0
Andreas Lorenzen <aplorenzen@gmail.com>
2018-05-16 03:04:06 +0200

    Fixed issue with prod environment value, gitUrl was missing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,7 @@ import env from './.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
+  gitUrl: 'https://github.com/aplorenzen/ngx-neo-website',
   serverUrl: 'https://api.chucknorris.io',
   defaultLanguage: 'English',
   supportedLanguages: [
