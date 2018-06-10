0fc1fed3696c4c86f7d331fc4ed49e6fde50ff9d
Andreas Lorenzen <aplorenzen@gmail.com>
2018-06-11 01:32:20 +0200

    Finally got the sliding animation working the way I want it

--- src/app/core/shell/navbar/navbar.component.ts
+++ src/app/core/shell/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, HostListener, Renderer2, ViewChild} from '@angular/core';
 
 import { I18nService } from '../../i18n.service';
+declare var $:any
 
 @Component({
   selector: 'app-navbar',
@@ -9,9 +10,10 @@ import { I18nService } from '../../i18n.service';
 })
 export class NavbarComponent {
 
-  isCollapsed = false;
+  @ViewChild('navigation') navigation: ElementRef;
 
-  constructor(private i18nService: I18nService) {}
+  constructor(private i18nService: I18nService,
+              private elementRef: ElementRef) {}
 
   setLanguage(language: string) {
     this.i18nService.language = language;
@@ -23,5 +25,19 @@ export class NavbarComponent {
 
   get languages(): string[] {
     return this.i18nService.supportedLanguages;
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: any) {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      if (this.navigation.nativeElement.classList.contains('show')) {
+        // TODO: Need to clean this up and make it typescript
+        // From https://stackoverflow.com/questions/30623825/how-to-use-jquery-with-angular?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+        // console.log('clicked outside');
+        // const collapseElement: HTMLElement = document.getElementById('collapse-element') as HTMLElement;
+        // collapseElement.collapse('hide');
+        $('.navbar-collapse').collapse('hide');
+      }
+    }
   }
 }
