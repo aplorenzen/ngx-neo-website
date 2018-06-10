8d0a74c3009ef5a636232778416231c6a89b68da
Andreas Lorenzen <aplorenzen@gmail.com>
2018-06-11 01:36:35 +0200

    Fixed tsconfig violations

--- src/app/core/shell/navbar/navbar.component.ts
+++ src/app/core/shell/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import {Component, ElementRef, HostListener, Renderer2, ViewChild} from '@angular/core';
 
 import { I18nService } from '../../i18n.service';
-declare var $:any
+declare var $: any;
 
 @Component({
   selector: 'app-navbar',
@@ -32,7 +32,8 @@ export class NavbarComponent {
     if (!this.elementRef.nativeElement.contains(event.target)) {
       if (this.navigation.nativeElement.classList.contains('show')) {
         // TODO: Need to clean this up and make it typescript
-        // From https://stackoverflow.com/questions/30623825/how-to-use-jquery-with-angular?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+        /* From https://stackoverflow.com/questions/30623825/how-to-use-jquery-with-angular?utm_medium=organic&utm_sourc
+        e=google_rich_qa&utm_campaign=google_rich_qa */
         // console.log('clicked outside');
         // const collapseElement: HTMLElement = document.getElementById('collapse-element') as HTMLElement;
         // collapseElement.collapse('hide');
