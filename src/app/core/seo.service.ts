33a11b7cf305814b328975f1de5f99039e2849cd
Andreas Lorenzen <aplorenzen@gmail.com>
2018-06-13 20:40:56 +0200

    Fixed seo service so that it can be pre rendered with angular universal (no use of location, document or window)

--- src/app/core/seo.service.ts
+++ src/app/core/seo.service.ts
@@ -1,15 +1,17 @@
 import { Inject, Injectable } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { DOCUMENT, Location } from '@angular/common';
+import { environment } from '@env/environment';
 
 @Injectable()
 export class SeoService {
 
-  constructor(@Inject(DOCUMENT) private rootDoc: Document) { }
+  constructor(@Inject(DOCUMENT) private rootDoc: Document,
+              private location: Location) { }
 
   createLinkForCanonicalURL() {
     const link: HTMLLinkElement = this.rootDoc.createElement('link');
     link.setAttribute('rel', 'canonical');
-    link.setAttribute('href', this.rootDoc.URL);
+    link.setAttribute('href', this.location.normalize(environment.deploymentRootUrl) + this.location.path());
     this.rootDoc.head.appendChild(link);
   }
 }
